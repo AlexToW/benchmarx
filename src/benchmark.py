c2ea02ac7d179e6b315c6a9c440b0b70937a3f97
Александр Тришин <aleksandrtrisin@MacBook-Pro-Aleksandr.local>
2023-04-06 17:41:21 +0300

    classmethod

--- src/benchmark.py
+++ src/benchmark.py
@@ -15,6 +15,7 @@ class TestFunctions:
     def __init__(self):
         pass
 
+    @classmethod
     def Rastrigin_f(self, x):
         """
         Global optimum: f(0, ..., 0) = 0
@@ -25,6 +26,7 @@ class TestFunctions:
         n = x.shape[0]
         return A * n + np.sum(x_i ** 2 - A * np.cos(2 * np.pi * x_i) for x_i in x)
 
+    @classmethod
     def Rastrigin_grad_f(self, x):
         """
         Gradient of Rastrigin function
@@ -34,6 +36,7 @@ class TestFunctions:
         A = 10
         return np.array(2*x_i + 2*np.pi*A*np.sin(2*np.pi*x_i) for x_i in x)
 
+    @classmethod
     def Ackley_f(self, z):
         """
         Global optimum: f(0, 0) = 0
@@ -45,7 +48,7 @@ class TestFunctions:
         return -20 * np.exp(-0.2 * np.sqrt(0.5 * (x ** 2 + y ** 2))) - np.exp(
             0.5 * (np.cos(2 * np.pi * x) + np.cos(2 * np.pi * y))) + np.e + 20
     
-
+    @classmethod
     def Ackley_grad_f(self, z):
         """
         Gradient of Ackley function
@@ -60,6 +63,7 @@ class TestFunctions:
         df_y = 2 * y * np.exp(-0.2*sqr) / sqr + np.pi * np.sin(2*np.pi*y)*np.exp(trig)
         return np.array([df_x, df_y])
 
+    @classmethod
     def Sphere_f(self, x):
         """
         Global optimum: f(0, ..., 0) = 0
@@ -68,6 +72,7 @@ class TestFunctions:
         """
         return np.sum(x_i ** 2 for x_i in x)
     
+    @classmethod
     def Sphere_grad_f(self, x):
         """
         Gradient of Sphere function
@@ -76,6 +81,7 @@ class TestFunctions:
         """
         return np.array(2*x_i for x_i in x)
 
+    @classmethod
     def Rosenbrock_f(self, x):
         """
         Global optimum: f(1,..., 1) = 0
@@ -89,6 +95,7 @@ class TestFunctions:
 
         return res
     
+    @classmethod
     def Rosenbrock_grad_f(self, x):
         """
         Gradient of Rosenbrock function
@@ -109,6 +116,7 @@ class TestFunctions:
 
         return grad
 
+    @classmethod
     def Beale_f(self, z):
         """
         Global optimum: f(3, 0.5) = 0
@@ -120,6 +128,7 @@ class TestFunctions:
         y = z[1]
         return (1.5 - x - x * y) ** 2 + (2.25 - x + x * y ** 2) ** 2 + (2.625 - x + x * y ** 3) ** 2
     
+    @classmethod
     def Beale_grad_f(self, z):
         """
         Gradient of Beale function
@@ -136,6 +145,7 @@ class TestFunctions:
         df_y = 2*x*(a1 + a2 + a3)
         return np.array([df_x, df_y])
 
+    @classmethod
     def Goldstein_Price_f(self, z):
         """
         Global optimum: f(0, -1) = 3
@@ -147,6 +157,7 @@ class TestFunctions:
         return (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2)) * (
                     30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x ** 2 + 48 * y - 36 * x * y + 27 * y ** 2))
     
+    @classmethod
     def Goldstein_Price_grad_f(self, z):
         '''
         Gradient of Goldstein-Price function
@@ -157,6 +168,7 @@ class TestFunctions:
         y = z[1]
         return np.array([24* (8* x**3 - 4* x**2* (9* y + 4) + 6* x* (9* y**2 + 8* y + 1) - 9* y* (3* y**2 + 4* y + 1))* ((3* x**2 + 2* x* (3* y - 7) + 3* y**2 - 14* y + 19)* (x + y + 1)**2 + 1) + 12* (x**3 + x**2* (3* y - 2) + x* (3* y**2 - 4* y - 1) + y**3 - 2* y**2 - y + 2)* ((12* x**2 - 4* x* (9* y + 8) + 3* (9* y**2 + 16* y + 6))* (2* x - 3* y)**2 + 30), 12* (x**3 + x**2* (3* y - 2) + x* (3* y**2 - 4* y - 1) + y**3 - 2* y**2 - y + 2)* ((12* x**2 - 4* x* (9* y + 8) + 3* (9* y**2 + 16* y + 6))* (2* x - 3* y)**2 + 30) - 36* (8* x**3 - 4* x**2* (9* y + 4) + 6* x* (9* y**2 + 8* y + 1) - 9* y* (3* y**2 + 4* y + 1))* ((3* x**2 + 2* x* (3* y - 7) + 3* y**2 - 14* y + 19)* (x + y + 1)**2 + 1)])
 
+    @classmethod
     def Booth_f(self, z):
         """
         Global optimum: f(1, 3) = 0
@@ -168,6 +180,7 @@ class TestFunctions:
         y = z[1]
         return (x + 2 * y - 7) ** 2 + (2 * x + y - 5) ** 2
     
+    @classmethod
     def Booth_grad_f(self, z):
         '''
         Gradient of Booth function
@@ -180,6 +193,7 @@ class TestFunctions:
         df_y = 8*x + 10*y - 38
         return np.array([df_x, df_y])
 
+    @classmethod
     def Bukin_f(self, z):
         """
         Global optimum: f(-10, 1) = 0
@@ -192,6 +206,7 @@ class TestFunctions:
         y = z[1]
         return 100 * np.sqrt(np.abs(y - 0.01 * x ** 2)) + 0.01 * np.abs(x + 10)
 
+    @classmethod
     def Bukin_grad_f(self, z):
         '''
         Gradient of Bukin function
@@ -207,6 +222,7 @@ class TestFunctions:
         df_y = 50*sign(tok) / np.sqrt(abs(tok))
         return np.array([df_x, df_y])
 
+    @classmethod
     def Matyas_f(self, z):
         """
         Global optimum: f(0, 0) = 0
@@ -218,6 +234,7 @@ class TestFunctions:
         y = z[1]
         return 0.26 * (x ** 2 + y ** 2) - 0.28 * x * y
 
+    @classmethod
     def Matyas_grad_f(self, z):
         """
         Gradient of Matyas function
@@ -229,6 +246,7 @@ class TestFunctions:
         y = z[1]
         return np.array([0.52*x - 0.48*y, 0.52*y - 0.48*x])
 
+    @classmethod
     def Levi_f(self, z):
         """
         Global optimum: f(1, 1) = 0
@@ -241,6 +259,7 @@ class TestFunctions:
         return np.sin(3 * np.pi * x) ** 2 + (x - 1) ** 2 * (1 + np.sin(3 * np.pi * y) ** 2) + (y - 1) ** 2 * (
                     1 + np.sin(2 * np.pi * y) ** 2)
 
+    @classmethod
     def Levi_grad_f(self, z):
         """
         Gradient of Levi function
@@ -254,6 +273,7 @@ class TestFunctions:
         df_y = (x-1)**2*(1 + 3*np.pi*np.sin(6*np.pi*y)) + 2*(y-1)*(1+(np.sin(3*np.pi*y)**2)) + (y-1)**2*(1+2*np.pi*np.cos(4*np.pi*y))
         return np.array([df_x, df_y])
 
+    @classmethod
     def Three_hump_camel_f(self, z):
         """
         Global optimum: f(0, 0) = 0
@@ -265,6 +285,7 @@ class TestFunctions:
         y = z[1]
         return 2 * x ** 2 - 1.05 * x ** 4 + x ** 6 / 6 + x * y + y ** 2
     
+    @classmethod
     def Three_hump_camel_grad_f(self, z):
         """
         Gradient of Three hump camel function
@@ -278,6 +299,7 @@ class TestFunctions:
         df_y = x + 2*y
         return np.array([df_x, df_y])
 
+    @classmethod
     def Easom_f(self, z):
         """
         Global optimum: f(pi, pi) = -1
@@ -289,6 +311,7 @@ class TestFunctions:
         y = z[1]
         return -np.cos(x) * np.cos(y) * np.exp(-((x - np.pi) ** 2 + (y - np.pi) ** 2))
 
+    @classmethod
     def Easom_grad_f(self, z):
         """
         Gradient of Easom function
--- src/draw_tools.py
+++ src/draw_tools.py
@@ -1,4 +1,9 @@
 
 class DrawTools:
+    trajectory_flag: bool = False
     def __init__(self):
+        pass
+
+    @classmethod
+    def plot_3d_function(self):
         pass

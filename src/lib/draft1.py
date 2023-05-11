075e62bff3ffe2659148a349af65c6c41456fcfd
Александр Тришин <aleksandrtrisin@MacBook-Pro-Aleksandr-2.local>
2023-05-11 12:40:20 +0300

    draft1

--- src/lib/draft1.py
+++ src/lib/draft1.py
@@ -31,6 +31,7 @@ def run_experiment():
                     'tol': 1e-9,
                     'maxiter': maxiter,
                     'stepsize' : 1e-1,
+                    'acceleration': False,
                     'label': 'GD_const'
                 },
                 'GRADIENT_DESCENT_adaptive_step': {
@@ -38,6 +39,7 @@ def run_experiment():
                     'tol': 1e-9,
                     'maxiter': maxiter,
                     'stepsize' : lambda iter_num:  1 / (iter_num + 9),
+                    'acceleration': False,
                     'label': 'GD_1/(k+9)'
                 },
                 
@@ -48,6 +50,7 @@ def run_experiment():
                     'stepsize' : 0.01,
                     'linesearch': 'backtracking',
                     'condition': 'armijo',
+                    'acceleration': False,
                     'label': 'GD_armijo'
                 },
                 'GRADIENT_DESCENT_goldstein_step': {
@@ -57,7 +60,45 @@ def run_experiment():
                     'stepsize' : 0.01,
                     'linesearch': 'backtracking',
                     'condition': 'goldstein',
+                    'acceleration': False,
                     'label': 'GD_goldstein'
+                },
+                'ArmijoSGD': {
+                    'x_init': x_init,
+                    'tol': 1e-9,
+                    'maxiter': maxiter,
+                    'label': 'Armijo SGD'
+                }
+            }
+        ],
+        metrics=[
+            "nit",
+            "history_x",
+            "history_f",
+            "history_df"
+        ],
+    )
+    result = benchmark.run()
+    result.save("./draft1/draft1.json")
+
+
+def run_experiment_small():
+    n = 6
+    key = jax.random.PRNGKey(758493)
+    x_init = jax.random.uniform(key, shape=(n,))
+    problem = QuadraticProblem(n=n)
+    L = float(jnp.max(jnp.real(jnp.linalg.eigh(problem.A)[0])))
+    maxiter = 250
+    benchmark = Benchmark(
+        runs=1000,
+        problem=problem,
+        methods=[
+            {
+                'ArmijoSGD': {
+                    'x_init': x_init,
+                    'tol': 1e-9,
+                    'maxiter': maxiter,
+                    'label': 'Armijo SGD'
                 }
             }
         ],
@@ -78,11 +119,12 @@ def draw():
         dir_path='draft1'
     )
     plotter.plot(log=True)
+    #plotter.plot()
 
 
 def _main():
     logging.getLogger().setLevel(logging.INFO)
-    #run_experiment()
+    #run_experiment_small()
     draw()
 
 

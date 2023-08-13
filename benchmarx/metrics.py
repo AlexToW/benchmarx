546f9b24e16daae2739166760e90d3770a334c32
AlexToW <trishin.as@phystech.edu>
2023-08-13 19:56:07 +0300

    nfev, njev, nhev

--- benchmarx/metrics.py
+++ benchmarx/metrics.py
@@ -2,25 +2,24 @@ import logging
 from typing import List, Callable
 
 
-
 class Metrics:
     """
     Metrics to track as the method runs and to save to a json file:
 
     [1]	x	    every json-file must include this metric
-    [2]	f	    objective function value 
+    [2]	f	    objective function value
     [3]	grad	gradient of the objective function
-    [4]	nit 	total number of iteration -- every json-file must include 
+    [4]	nit 	total number of iteration -- every json-file must include
                         this metric!
     [5]	nfev 	number of the objective function evaluations
     [6]	njev 	number of the objective function's gradient evaluations
     [7]	nhev	number of the objective function's hessian evaluation
-    [8] time    At the k-th iteration, the value of the time metric is the 
+    [8] time    At the k-th iteration, the value of the time metric is the
                 time elapsed from the start of the method to the k-th iteration.
 
-    Metrics to plot: 
-    (to plot a metric, it is not necessary for the corresponding 
-    metric to be tracked over the course of the method. 
+    Metrics to plot:
+    (to plot a metric, it is not necessary for the corresponding
+    metric to be tracked over the course of the method.
     Each of these metrics can be calculated along the method trajectory.)
 
         label		value
@@ -30,35 +29,20 @@ class Metrics:
     [4]	grad_norm	||grad||
     [5]	x_norm		||x||
 
-    Note: if one of the compulsory metrics to track is not 
-    contained in the list of metrics to track (it is set when 
-    the benchmark object is created) then this compulsory metric 
+    Note: if one of the compulsory metrics to track is not
+    contained in the list of metrics to track (it is set when
+    the benchmark object is created) then this compulsory metric
     will be added to metrics to track.
     """
-    compulsory_metrics_to_track = [
-        "x",
-        "nit"
-    ]
-    metrics_to_track = [
-        "x",
-        "f",
-        "grad",
-        "nit",
-        "nfev",
-        "njev",
-        "nhev",
-        "time"
-    ]
-    metrics_to_plot = [
-        "x_gap",
-        "f",
-        "f_gap",
-        "grad_norm",
-        "x_norm"
-    ]
+
+    compulsory_metrics_to_track = ["x", "nit"]
+    metrics_to_track = ["x", "f", "grad", "nit", "nfev", "njev", "nhev", "time"]
+    metrics_to_plot = ["x_gap", "f", "f_gap", "grad_norm", "x_norm"]
+    metrics_to_track_now = ["x", "nfev", "nhev", "njev", "time"]
+
     def __init__(self) -> None:
         pass
-    
+
     @staticmethod
     def check_metrics_to_track(metrics_to_check: List[str]):
         for metric in metrics_to_check:
@@ -66,18 +50,18 @@ class Metrics:
                 logging.warning(
                     msg=f"Metric '{metric}' is not contained in default metrics_to_track list. Use CustomMetric instead to specify your own metric."
                 )
-    
+
     @staticmethod
     def fix_metrics_to_track(metrics_to_fix: List[str]) -> List[str]:
         """
-        Add compulsory metrics and remove metrics that are 
+        Add compulsory metrics and remove metrics that are
         not contained in self.metrics_to_track.
         """
         fixed_metrics = Metrics.compulsory_metrics_to_track
         for metric in metrics_to_fix:
             if metric in Metrics.metrics_to_track:
                 fixed_metrics.append(metric)
-        
+
         return fixed_metrics
 
     @staticmethod
@@ -88,23 +72,45 @@ class Metrics:
                     msg=f"Metric '{metric}' is not contained in default metrics_to_plot list. Use CustomMetric instead to specify your own metric."
                 )
 
+    if False:
+        @staticmethod
+        def split_metrics(metrics_to_split: List[str]):
+            """
+            Split metrics on two parts:
+            - metrics to track as the method works: "x", "nfev", "nhev", "njev", "time" (if specified)
+            - metrics that will be calculated at the end of method: all metrics, except "x", ...,  "time" (if specified)
+            """
+            metrics_now = ["x", "nfev", "nhev", "njev", "time"]
+            metrics_to_track_now = [
+                metric for metric in metrics_now if metric in metrics_to_split
+            ]
+            if "x" not in metrics_to_track_now:
+                metrics_to_track_now.append("x")
+
+            metrics_to_track_after = [
+                metric for metric in metrics_to_split if metric not in metrics_now
+            ]
+
+            return metrics_to_track_now, metrics_to_track_after
+
 
 class CustomMetric:
     """
     CustomMetric class allows to compute your own metric to plot.
-    It is assumed that the metric is calculated using points from 
-    the method trajectory, i.e. function func is real-valued and 
-    takes as an argument a point from R^d, where d is dimensionality 
-    of the problem. 
-    The step parameter is responsible for the frequency of func 
-    calculation. 
-    self.func will be calculated at each iteration whose number 
+    It is assumed that the metric is calculated using points from
+    the method trajectory, i.e. function func is real-valued and
+    takes as an argument a point from R^d, where d is dimensionality
+    of the problem.
+    The step parameter is responsible for the frequency of func
+    calculation.
+    self.func will be calculated at each iteration whose number
     is a multiple of self.step.
     """
+
     def __init__(self, func: Callable, label: str, step: int = 1) -> None:
         self.func = func
         self.label = label
         self.step = step
-    
+
     def __str__(self) -> str:
         return self.label

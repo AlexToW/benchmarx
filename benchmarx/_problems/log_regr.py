c9b91194b320760060fe4b80e7c6ee0834c213fe
AlexToW <trishin.as@phystech.edu>
2023-08-14 16:00:01 +0300

    logistic regression problem

--- benchmarx/_problems/log_regr.py
+++ benchmarx/_problems/log_regr.py
@@ -19,15 +19,11 @@ class LogisticRegression(Problem):
     result = benchmark.run()
     """
 
-    def __init__(
-        self,
-        problem_type: str = "mushrooms"
-	) -> None:
+    def __init__(self, problem_type: str = "mushrooms") -> None:
         super().__init__(
-            info=f"Logistic Regression problem on {problem_type} dataset",
-            func=self.f
+            info=f"Logistic Regression problem on {problem_type} dataset", func=self.f
         )
-        
+
         if problem_type == "mushrooms":
             self.problem_type = problem_type
 
@@ -36,13 +32,14 @@ class LogisticRegression(Problem):
             X, y = data[0].toarray(), data[1]
             y = 2 * y - 3
 
-            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, shuffle=True)
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+                X, y, train_size=0.8, test_size=0.2, shuffle=True
+            )
             self.n_train, self.d_train = self.X_train.shape
             self.n_test, self.d_test = self.X_test.shape
 
         else:
             raise ValueError(f"Unknown problem type: {problem_type}")
-    
 
     @jax.jit
     def LogLoss(self, w, X, y):
@@ -70,18 +67,10 @@ class LogisticRegression(Problem):
         Objective function: log loss on train
         """
         return self.LogLoss(w=w, X=self.X_train, y=self.y_train)
-    
+
     @jax.jit
     def grad(self, w, *args, **kwargs):
         """
         log loss gradient on train
         """
         return self.GradLogLoss(w=w, X=self.X_train, y=self.y_train)
-
-
-
-            
-
-
-
-
